1bf8915b87a9c59b019f11649bba1081fa510245
maziggy <mz@v8w.de>
2026-02-27 09:48:31 +0100

    Updated test_backend.sh

--- test_backend.sh
+++ test_backend.sh
@@ -4,8 +4,8 @@ cd backend
 ruff check && ruff format --check
 
 #if [ "$1" = "--full" ]; then
-#  ../venv/bin/python3 -m pytest tests/ -v -n 14
+../venv/bin/python3 -m pytest tests/ -v -n 30
 #else
-../venv/bin/python3 -m pytest tests/ -v -n 14 --ignore=tests/unit/services/test_bambu_ftp.py
+#../venv/bin/python3 -m pytest tests/ -v -n 30 --ignore=tests/unit/services/test_bambu_ftp.py
 #fi
 #cd ..
